f1a8fd024e682cf722b3eeddeba7e4b65846b356
SongYang <songyang2088@126.com>
2016-05-31 14:07:47 +0800

    Choose picture test

---
+++ 001_FeatureGuide/choosepictest/src/main/java/com/nollec/choosepictest/MainActivity.java
@@ -0,0 +1,31 @@
+package com.nollec.choosepictest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button takePhoto;
+    private ImageView picture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        takePhoto = (Button) findViewById(R.id.take_photo);
+        picture = (ImageView) findViewById(R.id.picture);
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+
+    }
+}
